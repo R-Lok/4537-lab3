335e735e91238484d022d495b0f5f5d1a0765602
Ryan Lok <rlok.pc@gmail.com>
2025-01-31 19:12:07 -0800

    Abstract lab3 code into module

---
+++ modules/l3.js
@@ -0,0 +1,60 @@
+const lang = require('../lang/en/en.json')
+const dt = require('./utils')
+const fs = require('fs')
+
+function handle_l3(reqParams, req, res) {
+
+    if(reqParams.pathname === '/l3/getDate/') {
+        const str = lang.greetingFormat.replace('%1', reqParams.query.name ? reqParams.query.name : lang.greetingDefaultName) + dt.getDate() 
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end(`<p style='color: blue;'> ${str} </p>`)
+    } 
+
+    else if(reqParams.pathname === '/l3/writeFile/') 
+    {
+        handleWrite(req, res, reqParams.query.text)
+    }
+    
+    else if(reqParams.pathname.includes('/l3/readFile/')) 
+    {
+        const fileName = req.url.split('/readFile/')[1]
+
+        handleRead(req, res, fileName)
+    }
+    else
+    {
+        res.writeHead(404, {'Content-type': 'text/html'})
+        res.end(`<p> ${lang.resourceNotFound} </p>`)
+    }
+}
+
+function handleWrite(req, res, arg) {
+    if (!arg) {
+        res.writeHead(400, { 'Content-Type': 'text/html' });
+        res.end(`<p>${lang.badRequestNoArg} "text"</p>`);
+    } else {
+        fs.appendFile('file.txt', arg, (err) => {
+            if (err) {
+                res.writeHead(500, {'Content-type': 'text/html'})
+                res.end(`<p> ${lang.internalServerError}`)
+            } else {
+                res.writeHead(200, {'Content-type': 'text/html'})
+                res.end(`<p> ${lang.writeOk} </p>`)
+            }
+        })
+    }
+}
+
+function handleRead(req, res, fileName) {
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+        if(err) {
+            res.writeHead(err.errno === -2 ? 404:500, {'Content-type': 'text/html'})
+            res.end(err.errno === -2 ? `<p> ${lang.resourceNotFound}${fileName != ''? ': ' + fileName : ''} ` : `<p> ${lang.internalServerError} </p>`)
+        } else {
+            res.writeHead(200, {'Content-type': 'text/plain'})
+            res.end(`${data}`)
+        }
+    })
+}
+
+module.exports = {handle_l3}
--- server.js
+++ server.js
@@ -1,33 +1,18 @@
  
  //import node libraries
 const http = require('http');
-const fs = require('fs')
 const url = require('url')
 
 //import my own modules/localizations
-const dt = require('./modules/utils')
 const lang = require('./lang/en/en.json')
+const l3 = require('./modules/l3')
 
 //set up server endpoints
 const server = http.createServer(function (req, res) {
     const reqParams = url.parse(req.url, true)
 
-    if(reqParams.pathname === '/l3/getDate/') {
-        const str = lang.greetingFormat.replace('%1', reqParams.query.name ? reqParams.query.name : lang.greetingDefaultName) + dt.getDate() 
-        res.writeHead(200, {'Content-type': 'text/html'})
-        res.end(`<p style='color: blue;'> ${str} </p>`)
-    } 
-
-    else if(reqParams.pathname === '/l3/writeFile/') 
-    {
-        handleWrite(req, res, reqParams.query.text)
-    }
-    
-    else if(reqParams.pathname.includes('/l3/readFile/')) 
-    {
-        const fileName = req.url.split('/readFile/')[1]
-
-        handleRead(req, res, fileName)
+    if(req.url.startsWith('/l3')) {
+        l3.handle_l3(reqParams, req, res)
     }
     else
     {
@@ -40,32 +25,3 @@ const server = http.createServer(function (req, res) {
 server.listen(8000);
 
 console.log("Server is running and listening")
-
-function handleWrite(req, res, arg) {
-    if (!arg) {
-        res.writeHead(400, { 'Content-Type': 'text/html' });
-        res.end(`<p>${lang.badRequestNoArg} "text"</p>`);
-    } else {
-        fs.appendFile('file.txt', arg, (err) => {
-            if (err) {
-                res.writeHead(500, {'Content-type': 'text/html'})
-                res.end(`<p> ${lang.internalServerError}`)
-            } else {
-                res.writeHead(200, {'Content-type': 'text/html'})
-                res.end(`<p> ${lang.writeOk} </p>`)
-            }
-        })
-    }
-}
-
-function handleRead(req, res, fileName) {
-    fs.readFile(fileName, 'utf-8', (err, data) => {
-        if(err) {
-            res.writeHead(err.errno === -2 ? 404:500, {'Content-type': 'text/html'})
-            res.end(err.errno === -2 ? `<p> ${lang.resourceNotFound}${fileName != ''? ': ' + fileName : ''} ` : `<p> ${lang.internalServerError} </p>`)
-        } else {
-            res.writeHead(200, {'Content-type': 'text/plain'})
-            res.end(`${data}`)
-        }
-    })
-}
